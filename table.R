ada88db777f7eeff590f35dd2653a3c338058f98
sophia-mangrubang <mangrsop@uw.edu>
2023-02-17 21:47:42 -0800

    table made

--- table.R
+++ table.R
@@ -1,1 +1,25 @@
 
+kidz_bop_df <- read.csv("https://raw.githubusercontent.com/the-pudding/data/master/kidz-bop/KB_censored-lyrics.csv", stringsAsFactors = FALSE)
+
+library("dplyr")
+library("stringr")
+View(kidz_bop_df)
+
+# Created a new DF with the desired columns and summarized count column
+KB_data_df <- kidz_bop_df %>% group_by(badword, ogArtist, category, year) %>% summarize(total_count = sum(count))
+
+# Made a variable for how many rows in the new df
+num_categories <- nrow(KB_data_df)
+
+# Created a separate dataframe to count up how many times the categories
+category_count <- data.frame(KB_data_df$category)
+category_count <- kidz_bop_df %>% count(category)
+category_count <- category_count %>% mutate(category_prop = n / num_categories)
+View(category_count)
+
+# Created the final table df by joining the category_count df and t KB_data_df together, renamed and reordered the columns
+summary_table <- left_join(KB_data_df, category_count, by= "category")
+colnames(summary_table)[6] = "category_count"
+summary_table <- summary_table[,c("badword", "ogArtist", "total_count", "heyear", "category", "category_count", "category_prop")]
+View(summary_table)
+
