7b35a7c957ca77a758e83be1a5d17ad999c649b8
erixaaxo <erica.bergen2002@gmail.com>
2023-02-18 13:29:12 -0800

    Nima chart

--- chart3-NR.R
+++ chart3-NR.R
@@ -1,1 +1,17 @@
+library("dplyr")
+library("ggplot2")
+library("stringr")
+
+# Reading CSV file
+kidz_bop_df <- read.csv("https://raw.githubusercontent.com/the-pudding/data/master/kidz-bop/KB_censored-lyrics.csv", stringsAsFactors = FALSE)
+
+
+# Grouping Bad Words with Category
+bad_words_per_category <- kidz_bop_df %>% group_by(category) %>% summarize(bad_words_per_category = sum(count, na.rm = T))
+
+
+
+# Creating Graph
+ggplot(data = bad_words_per_category, aes(x = category, y = bad_words_per_category)) +
+  geom_bar(stat = "identity")
 
