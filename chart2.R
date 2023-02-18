80fe6fb27a8781b8d7e798aca12c63afb8484da3
jechow <jechow@uw.edu>
2023-02-18 10:50:08 -0800

    Joshua chart

--- chart2.R
+++ chart2.R
@@ -1,1 +1,11 @@
 
+# Runs the code to make the table from the file table.R
+kidz_bop_df <- read.csv("https://raw.githubusercontent.com/the-pudding/data/master/kidz-bop/KB_censored-lyrics.csv", stringsAsFactors = FALSE)
+library("dplyr")
+
+number_of_censors <- kidz_bop_df %>% group_by(year) %>% summarise(count = sum(count))
+
+library(ggplot2)
+ggplot() +
+  geom_line(aes(x=number_of_censors$year, y = number_of_censors$count))
+
