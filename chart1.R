7c5f5b3b043d226442dc8b6cbd1cc6fc82078528
erixaaxo <erica.bergen2002@gmail.com>
2023-02-19 14:54:42 -0800

    chart erica

--- chart1.R
+++ chart1.R
@@ -5,7 +5,7 @@ library("stringr")
 kb_data <- read.csv("https://raw.githubusercontent.com/the-pudding/data/master/kidz-bop/KB_group-overiew.csv", stringsAsFactors = FALSE)
 
 ggplot(data = kb_data, mapping = aes(x = category, y = pctCensored)) +
-  geom_point() +
+  geom_point()
   
 
 
