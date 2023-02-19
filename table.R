50b080a5190b8379ffa94d30d99544ed603b7551
sophia-mangrubang <mangrsop@uw.edu>
2023-02-19 11:13:20 -0800

    final table

--- table.R
+++ table.R
@@ -8,7 +8,13 @@ View(kidz_bop_df)
 # Created a new DF with the desired columns and summarized count column
 kb_our_df <- kidz_bop_df %>% group_by(badword, ogArtist, songName, category, year) %>% summarize(total_count = sum(count))
 View(kb_our_df)
-# Made a variable for how many rows in the original kidz_bop_df
+
+new_df <- kidz_bop_df  %>% group_by(badword, category) %>% summarize(total_count = sum(count))
+View(new_df)
+
+new_df <- new_df %>% group_by(badword, category, total_count) %>% filter(total_count >= 74) %>% arrange(-total_count)
+
+# Made a variable for how may rows in the original kidz_bop_df
 num_categories <- nrow(kidz_bop_df)
 
 # Created a separate dataframe to count up the proportion each category makes up of the total entries in the original df
@@ -18,8 +24,8 @@ category_count <- category_count %>% mutate(category_prop = n / num_categories)
 View(category_count)
 
 # Created the final table df by joining the category_count df and t KB_data_df together, renamed and reordered the columns
-summary_table <- left_join(kb_our_df, category_count, by= "category")
-colnames(summary_table)[6] = "category_count"
-summary_table <- summary_table[,c("badword", "songName", "ogArtist", "total_count", "year", "category", "category_count", "category_prop")]
+summary_table <- left_join(new_df, category_count, by= "category")
+colnames(summary_table)[4] = "category_count"
+summary_table <- summary_table[,c("badword", "category", "total_count","category_count", "category_prop")]
 View(summary_table)
 
